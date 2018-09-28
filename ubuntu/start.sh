63e51b65307b549e8058166be5b8044b43a89b49
Kyle Harding <kylemharding@gmail.com>
2018-09-28 18:06:41 +0000

    replace cloud9 with local ubuntu image

---
+++ ubuntu/start.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# set timezone with TZ
+# eg. TZ=America/Toronto
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# generate ssh key if one does not exist
+if [ ! -f "/root/.ssh/id_rsa" ]
+then
+	ssh-keygen -q -N '' -f "/root/.ssh/id_rsa"
+fi
+
+# generate ssh host keys
+/usr/bin/ssh-keygen -A
+
+# if a github username was provided and authorized_keys does not exist
+if [ -n "${GITHUB_USER}" ] && [ ! -e "/root/.ssh/authorized_keys" ]
+then
+	curl "https://github.com/${GITHUB_USER}.keys" > "/root/.ssh/authorized_keys"
+fi
+
+# set permissions on ssh dir
+chown -R root:root "/root/.ssh"
+chmod -R 700 "/root/.ssh"
+
+if [ "${INITSYSTEM}" != "on" ]
+then
+	# start sshd service in background
+	/usr/sbin/sshd -p 22 -D &
+	
+	# start docker daemon in the background
+	/usr/bin/dockerd ${DIND_OPTS} &
+fi
+
