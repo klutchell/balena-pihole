290a25263a98dbca1d5ad2c8f949c6dc7c419fb8
Kyle Harding <kylemharding@gmail.com>
2019-02-09 14:46:30 -0500

    avoid failing if down files do not exist

--- pihole/init/30-checktft.sh
+++ pihole/init/30-checktft.sh
@@ -2,6 +2,6 @@
 
 if [ -e /dev/fb1 ]; then
     s6-echo "/dev/fb1 exists. Enabling fbcp & PADD"
-    rm /etc/services.d/fbcp/down
-    rm /etc/services.d/padd/down
+    rm /etc/services.d/fbcp/down || true
+    rm /etc/services.d/padd/down || true
 fi
