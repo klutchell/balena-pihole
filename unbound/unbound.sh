71c369a23ed5806f4fcbab3d91650467d9fe3ade
Kyle Harding <kharding@marchnetworks.com>
2019-02-20 13:48:44 -0500

    download root hints during boot

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -xe
+#!/bin/sh
+
+set -xe
 
 # set timezone with TZ (eg. TZ=America/Toronto)
 if [ -n "${TZ}" ]
@@ -19,6 +21,12 @@ then
 	chown unbound:unbound /opt/unbound/etc/unbound/var
 fi
 
+# download the list of primary root servers.
+# unbound ships its own list but we can also download the most recent list and
+# update it whenever we think it is a good idea.
+# note: there is no point in doing it more often then every 6 months.
+curl -fsSL https://www.internic.net/domain/named.root -o /opt/unbound/etc/unbound/var/root.hints
+
 # update the root trust anchor for DNSSEC validation.
 # this tool exits with value 1 if the root anchor was updated using the
 # certificate or if the builtin root-anchor was used.
@@ -26,4 +34,9 @@ fi
 # possible with RFC5011 tracking, or if an error occurred.
 /opt/unbound/sbin/unbound-anchor -a /opt/unbound/etc/unbound/var/root.key || true
 
-exec /opt/unbound/sbin/unbound -d -c /opt/unbound/etc/unbound/unbound.conf
+if [ $# -eq 0 ]
+then
+	exec /opt/unbound/sbin/unbound -d -c /opt/unbound/etc/unbound/unbound.conf
+else
+	exec "$@"
+fi
