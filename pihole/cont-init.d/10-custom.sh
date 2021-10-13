37a1e1d589fe6a4abfe2bd1d34523af2ab0153f4
Kyle Harding <kyle@balena.io>
2021-10-13 10:48:58 -0400

    Add hosts entries for hostname and pihole
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- pihole/cont-init.d/10-custom.sh
+++ pihole/cont-init.d/10-custom.sh
@@ -3,13 +3,16 @@
 
 set -e
 
-pihole -a -p "${WEBPASSWORD}" || true
-
 while [ -z "$(ip -o -4 addr show dev "${INTERFACE}")" ]
 do
    echo "Waiting for IPv4 address on ${INTERFACE}..."
    sleep 5
 done
+
+pihole -a -p "${WEBPASSWORD}" || true
+
+echo "127.0.0.1 pihole" >> /etc/hosts
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
 # https://serverfault.com/a/817791
 # force dnsmasq to bind only the interfaces it is listening on
