d8080853c664df6cf88bac0f82cddccf12e885a3
Kyle Harding <kyle@balena.io>
2022-04-26 09:47:38 -0400

    Filter package rules by package name
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- postUpgrade.sh
+++ postUpgrade.sh
@@ -2,22 +2,13 @@
 
 set -e
 
-depName="${1}"
-newVersion="${2}"
-
-if [ "${depName}" != "pihole/pihole" ]
-then
-    echo "skipping dependency ${depName}!"
-    exit 0
-fi
-
 re='^0*([1-9][0-9]*)\.0*([1-9][0-9]*)\.0*([1-9][0-9]*)$'
 
-if [[ "${newVersion}" =~ $re ]]
+if [[ "${1}" =~ $re ]]
 then
     semver="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"
     sed "s/^version: .*$/version: ${semver}/" -i balena.yml
 else 
-    echo "${newVersion} is not a supported version string!"
+    echo "${1} is not a supported version string!"
     exit 1
 fi
