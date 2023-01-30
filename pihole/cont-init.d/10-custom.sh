15eb371fd1352a7c5001731f306bbafe8f3e35cc
Kyle Harding <kyle@balena.io>
2023-01-30 09:19:00 -0500

    Reduce DNS packet size for unbound to 1232
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- pihole/cont-init.d/10-custom.sh
+++ pihole/cont-init.d/10-custom.sh
@@ -5,6 +5,16 @@ set -e
 
 pihole -a -p "${WEBPASSWORD}" || true
 
+# check if we are using unbound as upstream DNS
+if [[ ${PIHOLE_DNS_%;*} =~ ^127\.0\.0\.1#5053$ ]]
+then
+   # https://docs.pi-hole.net/guides/dns/unbound/
+   # https://docs.pi-hole.net/ftldns/dnsmasq_warn/#reducing-dns-packet-size-for-nameserver-address-to-safe_pktsz
+   pkt_size=1232
+   echo "Reducing DNS packet size for nameserver ${PIHOLE_DNS_%;*} to ${pkt_size}..."
+   echo "edns-packet-max=${pkt_size}" > /etc/dnsmasq.d/99-edns.conf
+fi
+
 while [ -z "$(ip -o -4 addr show dev "${INTERFACE}")" ]
 do
    echo "Waiting for IPv4 address on ${INTERFACE}..."
